8bd0489e6d67a395c74e02bc6bfc97a169aa9383
arnobl <1052622+arnobl@users.noreply.github.com>
2019-10-21 19:11:57 +0200

    change(command): CommandsRegistry now publishes new commands. Beginning of the end of CmdHandler

--- src/main/java/io/github/interacto/instrument/InstrumentImpl.java
+++ src/main/java/io/github/interacto/instrument/InstrumentImpl.java
@@ -15,11 +15,12 @@
 package io.github.interacto.instrument;
 
 import io.github.interacto.binding.WidgetBinding;
-import io.github.interacto.undo.Undoable;
-import java.util.ArrayList;
-import java.util.List;
 import io.github.interacto.command.Command;
-import io.github.interacto.error.ErrorCatcher;
+import io.reactivex.disposables.Disposable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -27,7 +28,7 @@ import org.w3c.dom.Element;
  * The base class of an instrument.
  * @author Arnaud BLOUIN
  */
-public abstract class InstrumentImpl<T extends WidgetBinding> implements Instrument<T> {
+public abstract class InstrumentImpl<T extends WidgetBinding<? extends Command>> implements Instrument<T> {
 	/** Defines whether the instrument is activated. */
 	protected boolean activated;
 
@@ -37,6 +38,8 @@ public abstract class InstrumentImpl<T extends WidgetBinding> implements Instrum
 	/** Defined whether the instrument has been modified. */
 	protected boolean modified;
 
+	protected final Set<Disposable> disposables;
+
 	/**
 	 * Creates and initialises the instrument.
 	 */
@@ -45,8 +48,20 @@ public abstract class InstrumentImpl<T extends WidgetBinding> implements Instrum
 		activated = false;
 		modified = false;
 		bindings = new ArrayList<>();
+		disposables = new HashSet<>();
 	}
 
+	/**
+	 * The instrument can store disposable objects that will be flushed
+	 * on un-installation.
+	 * This method stores a disposable object.
+	 * @param disposable The disposable object to store. Nothing done if null.
+	 */
+	protected void addDisposable(final Disposable disposable) {
+		if(disposable != null) {
+			disposables.add(disposable);
+		}
+	}
 
 	@Override
 	public int getNbWidgetBindings() {
@@ -68,10 +83,8 @@ public abstract class InstrumentImpl<T extends WidgetBinding> implements Instrum
 
 	/**
 	 * Initialises the bindings of the instrument.
-	 * @throws InstantiationException When a widget binding cannot instantiate its interaction.
-	 * @throws IllegalAccessException When a widget binding cannot instantiate its interaction.
 	 */
-	protected abstract void configureBindings() throws InstantiationException, IllegalAccessException;
+	protected abstract void configureBindings();
 
 
 	/**
@@ -81,18 +94,8 @@ public abstract class InstrumentImpl<T extends WidgetBinding> implements Instrum
 	public void addBinding(final T binding) {
 		if(binding != null) {
 			bindings.add(binding);
-			binding.setCmdHandler(this);
 			binding.setActivated(isActivated());
 		}
-	}
-
-	/**
-	 * Removes the given widget binding from the list of bindings of the instrument.
-	 * @param binding The widget binding to remove.
-	 * @return True: the given widget binding has been removed. False otherwise.
-	 */
-	public boolean removeBinding(final T binding) {
-		return binding != null && bindings.remove(binding);
 	}
 
 
@@ -113,11 +116,7 @@ public abstract class InstrumentImpl<T extends WidgetBinding> implements Instrum
 		activated = toBeActivated;
 
 		if(toBeActivated && !hasWidgetBindings()) {
-			try {
-				configureBindings();
-			}catch(final InstantiationException | IllegalAccessException ex) {
-				ErrorCatcher.INSTANCE.reportError(ex);
-			}
+			configureBindings();
 		}else {
 			bindings.forEach(binding -> binding.setActivated(toBeActivated));
 		}
@@ -154,45 +153,10 @@ public abstract class InstrumentImpl<T extends WidgetBinding> implements Instrum
 	public void uninstallBindings() {
 		bindings.forEach(binding -> binding.uninstallBinding());
 		bindings.clear();
-	}
-
-	@Override
-	public void onUndoableCleared() {
-		// Should be overridden.
-	}
-
-	@Override
-	public void onUndoableAdded(final Undoable undoable) {
-		// Should be overridden.
-	}
-
-	@Override
-	public void onUndoableUndo(final Undoable undoable) {
-		// Should be overridden.
-	}
-
-	@Override
-	public void onUndoableRedo(final Undoable undoable) {
-		// Should be overridden.
-	}
-
-	@Override
-	public void onCmdAdded(final Command cmd) {
-		// Should be overridden.
-	}
-
-	@Override
-	public void onCmdCancelled(final Command cmd) {
-		// Should be overridden.
-	}
-
-	@Override
-	public void onCmdExecuted(final Command cmd) {
-		// Should be overridden.
-	}
-
-	@Override
-	public void onCmdDone(final Command cmd) {
-		// Should be overridden.
+		disposables
+			.stream()
+			.filter(d -> !d.isDisposed())
+			.forEach(d -> d.dispose());
+		disposables.clear();
 	}
 }
